a361f77783d562ee286e2ecc2b5f70c583af1b2a
Julius Koronci <julius.koronci@gmail.com>
2024-02-25 14:49:59 +0100

    add test suits and storybook scaffolding

---
+++ lib/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import * as publicApi from '../index';
+
+describe('Public api', () => {
+  it('Should have a test export', () => {
+    expect(publicApi.test).toBeDefined();
+  });
+});

28a5d62cf4f2caee088742510963251790f2a040
martinababinska <martina.babinska.sk@gmail.com>
2024-04-30 19:54:03 +0200

    chore: disable react import rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,14 +9,14 @@ module.exports = {
     'plugin:storybook/recommended',
     'prettier',
   ],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'react'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/semi': 'off',
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
